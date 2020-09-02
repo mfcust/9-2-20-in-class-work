d859bf72a1fd93d6a237c43ca0f97c799ea8f768
mfcust <43455391+mfcust@users.noreply.github.com>
2020-09-01 23:17:26 -0700

    Create 9-2-20.py

---
+++ 9-2-20.py
@@ -0,0 +1,46 @@
+###9-2-20 work###
+
+###You can write comments like this!!###
+
+'''Or if your comments span multiple lines,
+like this
+and this
+and this, you can use three single quotes to comment out large blocks of code.'''
+
+
+
+#Math in python: +, -, /, * and print to the console
+
+
+
+#Calculate 100 divided by 2 and print to the console
+
+
+
+#Calculate 3 multiplied by 1,000 and print to the console
+
+
+
+#Calculate 50 minus 75 and print to the console
+
+
+
+#Calculate 100.5 plus 32.17864532983764862 and print to the console
+
+
+
+#Calculate 32 plus 8 multiplied by 6 and print to the console
+
+
+
+
+#Bonus problems
+#Calculate 2 to the power of 6 and print to the console
+
+
+
+#Calculate 40 divided by "Hello" and print to the console
+#Hint: If you get an error, why does that happen???
+
+
+
