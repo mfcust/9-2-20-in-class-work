5ed7bde34dd1c189d6876cc2f3e04382eebd271c
mfcust <43455391+mfcust@users.noreply.github.com>
2020-12-03 07:49:44 -0800

    Update operators_comments.py

--- operators_comments.py
+++ operators_comments.py
@@ -7,44 +7,59 @@ like this
 and this
 and this, you can use three single quotes to comment out large blocks of code.'''
 
-
-
-#Math in python: +, -, /, * and print to the console
-
-
-
-#Calculate 100 divided by 2 and print to the console
+#1) Write  a comment below by using the '#' symbol.
 
 
 
-#Calculate 3 multiplied by 1,000 and print to the console
 
 
 
-#Calculate 50 minus 75 and print to the console
+#2) Write a multi-line comment below by using three single quotes on each side ('''like this, but multiple lines''')
 
 
 
-#Calculate 100.5 plus 32.17864532983764862 and print to the console
 
 
 
-#Calculate 32 plus 8 multiplied by 6 and print to the console
+#### These are the most common operators for performing math in python: +(addition), -(subtraction), /(division), *(multiplication)####
+
+
+
+
+#3) Calculate 100 divided by 2 and print to the console
+
+
+
+#4) Calculate 3 multiplied by 1,000 and print to the console
+
+
+
+#5) Calculate 50 minus 75 and print to the console
+
+
+
+#6) Calculate 100.5 plus 32.17864532983764862 and print to the console
+
+
+
+#7) Calculate 32 plus 8, then multiply by 6 and print to the console
 
 
 
 
 #Bonus problems
-#Calculate 2 to the power of 6 and print to the console
+#1) Calculate 2 to the power of 6 and print to the console
 
 
 
-#Calculate 40 divided by "Hello" and print to the console
+
+#2) Calculate 40 divided by "Hello" and print to the console
 #Hint: If you get an error, why does that happen???
 
 
 
-#What is the % operator? Perform a calculation with it below
+
+#3) What is the % operator? Perform a calculation with it below
 
 
 
